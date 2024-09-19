665bc9824bc311955f0c1d7c9488e9771961e43c
Maker Zone <dev@makerzone.com.au>
2024-09-19 18:51:36 +1000

    Updated build.sh to correctly reference DisplayValueOLED library

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@
 # Set the board type (change this if you're using a different board)
 BOARD_TYPE="arduino:avr:uno"
 
+# Get the absolute path of the current working directory
+BASE_DIR=$(pwd)
+
 # Directory paths
-LIBRARY_PATH=$(pwd)
-EXAMPLES_PATH="$LIBRARY_PATH/examples"
+EXAMPLES_PATH="$BASE_DIR/examples"
+OLED_LIBRARY_PATH="$BASE_DIR"
+COMMON_LIBRARY_PATH="$HOME/Arduino/libraries/DisplayValueCommon"
 
 # Check if arduino-cli is installed
 if ! command -v arduino-cli &> /dev/null
@@ -18,7 +22,7 @@ fi
 for example in "$EXAMPLES_PATH"/*; do
     if [ -d "$example" ]; then
         echo "Compiling example: $example"
-        arduino-cli compile --fqbn $BOARD_TYPE "$example"
+        arduino-cli compile --fqbn $BOARD_TYPE --libraries "$OLED_LIBRARY_PATH,$COMMON_LIBRARY_PATH" "$example"
         
         if [ $? -ne 0 ]; then
             echo "Error: Compilation failed for $example"
@@ -27,7 +31,7 @@ for example in "$EXAMPLES_PATH"/*; do
             echo "Compilation successful for $example"
         fi
     fi
-
+    
 done
 
 
