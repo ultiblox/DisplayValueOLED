d09995894c9d5b7d079ce9c8aa9fa4824afd4439
Maker Zone <dev@makerzone.com.au>
2024-11-27 15:56:27 +1100

    Removing references to obsolete DisplayValueCommon

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ BASE_DIR=$(pwd)
 # Directory paths
 EXAMPLES_PATH="$BASE_DIR/examples"
 OLED_LIBRARY_PATH="$BASE_DIR"
-COMMON_LIBRARY_PATH="$HOME/Arduino/libraries/DisplayValueCommon"
 
 # Check if arduino-cli is installed
 if ! command -v arduino-cli &> /dev/null
@@ -22,7 +21,7 @@ fi
 for example in "$EXAMPLES_PATH"/*; do
     if [ -d "$example" ]; then
         echo "Compiling example: $example"
-        arduino-cli compile --fqbn $BOARD_TYPE --libraries "$OLED_LIBRARY_PATH,$COMMON_LIBRARY_PATH" "$example"
+        arduino-cli compile --fqbn $BOARD_TYPE --libraries "$OLED_LIBRARY_PATH" "$example"
         
         if [ $? -ne 0 ]; then
             echo "Error: Compilation failed for $example"
